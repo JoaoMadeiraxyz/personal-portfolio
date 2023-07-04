4b7cf4b861074a37703345c99eb0182bf3fa1062
Joao Victor <joao@contabilprimor.com.br>
2023-07-04 16:50:47 -0300

    updating page title

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      <title>Joao's Portfolio</title>
       <NavBar />
       <Banner />
       <Skills />
